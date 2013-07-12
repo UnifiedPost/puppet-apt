5fc7610a274a2b0cbb8550837e018aa36631dc4b
Jan Vansteenkiste <jan@vstone.eu>
2013-07-12 14:06:55 +0200

    Add support to add repositories using split up parameters in stead of just a source or the complete content

--- manifests/sources_list.pp
+++ manifests/sources_list.pp
@@ -1,20 +1,54 @@
 define apt::sources_list (
-  $ensure  = present,
-  $source  = false,
-  $content = false
+  $ensure       = present,
+  $source       = false,
+  $content      = false,
+  $target       = "/etc/apt/sources.list.d/${name}.list",
+  $uri          = undef,
+  $components   = undef,
+  $deb_src      = false,
+  $distribution = $::lsbdistcodename,
+  $key          = undef,
+  $keyserver    = undef,
+  $keysource    = undef,
+  $keycontent   = undef,
 ) {
 
-  if $source {
-    file {"/etc/apt/sources.list.d/${name}.list":
-      ensure => $ensure,
-      source => $source,
-      before => Exec['apt-get_update'],
-      notify => Exec['apt-get_update'],
+  include apt
+
+  if $key {
+    if ! defined(Apt::Key[$key]) {
+      apt::key { $key:
+        ensure => $ensure
+      }
+      if $keyserver  { Apt::Key[$key] { keyserver => $keyserver  } }
+      if $keysource  { Apt::Key[$key] { source    => $keysource  } }
+      if $keycontent { Apt::Key[$key] { content   => $keycontent } }
     }
-  } else {
-    file {"/etc/apt/sources.list.d/${name}.list":
+  }
+
+  if $source {
+    file {$target:
+      ensure  => $ensure,
+      source  => $source,
+      before  => Exec['apt-get_update'],
+      notify  => Exec['apt-get_update'],
+    }
+  } elsif $content {
+    file {$target:
       ensure  => $ensure,
       content => $content,
+      before  => Exec['apt-get_update'],
+      notify  => Exec['apt-get_update'],
+    }
+  }
+  else {
+    if $uri == undef or $components == undef {
+      fail('uri and components are required parameters when not using source or content.')
+    }
+
+    file {$target:
+      ensure  => $ensure,
+      content => template('apt/sources.list.erb'),
       before  => Exec['apt-get_update'],
       notify  => Exec['apt-get_update'],
     }
