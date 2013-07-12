5eb6df63aa1a38838bf391a3f1c64f76b6b04fbf
Jan Vansteenkiste <jan@vstone.eu>
2013-07-12 13:41:56 +0200

    Get rid of these silly globally scoped variables.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,39 +1,18 @@
 class apt::params {
 
-  $manage_preferences = $apt_manage_preferences ? {
-    ''      => true,
-    default => $apt_manage_preferences,
-  }
 
-  $manage_sourceslist = $apt_manage_sourceslist ? {
-    ''      => true,
-    default => $apt_manage_sourceslist,
-  }
+  $manage_preferences = true
+  $manage_sourceslist = true
 
-  $ignore_sourceslist = $apt_ignore_sourceslist ? {
-    ''      => '.placeholder',
-    default => $apt_ignore_sourceslist,
-  }
+  $ignore_sourceslist = '.placeholder'
 
   $keyring_package = $::lsbdistid ? {
     Debian => ['debian-keyring', 'debian-archive-keyring'],
     Ubuntu => 'ubuntu-keyring',
   }
 
-  $clean_minutes  = $apt_clean_minutes ? {
-    ''      => fqdn_rand(60),
-    default => $apt_clean_minutes,
-  }
+  $clean_minutes  = fqdn_rand(60)
+  $clean_hours    = '0'
+  $clean_monthday = fqdn_rand(28) + 1
 
-  $clean_hours    = $apt_clean_hours ? {
-    ''      => '0',
-    default => $apt_clean_hours,
-  }
-
-  $clean_monthday_rand = fqdn_rand(28) + 1
-
-  $clean_monthday = $apt_clean_mday ? {
-    ''      => $clean_monthday_rand,
-    default => $apt_clean_mday,
-  }
 }
